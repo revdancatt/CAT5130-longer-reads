49ca4411505f0cea2977756a0a6c66b387faab03
Rev Dan Catt <revdancatt@gmail.com>
2010-08-04 17:10:08 +0100

    fix error when rejecting article that has been queued

--- api/lr.article.reject.py
+++ api/lr.article.reject.py
@@ -25,6 +25,7 @@ apiUrl = urllib.unquote(args['apiUrl'])
 rows = db.GqlQuery("SELECT * FROM Items WHERE apiUrl = :1", apiUrl)
 for row in rows:
   row.unreviewed = 0
+  row.queued = 0
   row.rejected = 1
   row.last_update = datetime.now()
   row.put()
